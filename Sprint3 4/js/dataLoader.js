f2877120d3abfe44376758f11d5eda080f4b62e5
refocusing <bnrdelk@gmail.com>
2024-05-27 13:22:25 +0300

    Add files via upload

--- Sprint3 4/js/dataLoader.js
+++ Sprint3 4/js/dataLoader.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $.getJSON("../data.json", function(data){
         var welcomeText = data.welcomeText;
-        $("#welcome-text").append(
+        $("#data").append(
             "<h2>" + welcomeText.heading + "</h2>" +
             "<h3>" + welcomeText.subHeading + "</h3>"
         );
         $.each(welcomeText.paragraphs, function(index, paragraph) {
-            $("#welcome-text").append("<p>" + paragraph + "</p>");
+            $("#data").append("<p>" + paragraph + "</p>");
         });
     });
 });
